4c15f407e70ffbe4876eb409d5b469695d54d979
YashdalfTheGray <yash.kulshrestha@gmail.com>
2016-11-20 17:29:46 -0500

    added another test for dispatching an action

--- src/stores/app.spec.js
+++ src/stores/app.spec.js
@@ -1,9 +1,17 @@
 import test from 'tape';
 import app from './app';
+const { store, actions } = app;
 
 test('app store', t => {
     t.test('gets initial state', t => {
-        t.equal(app.store.getState().message, 'Redux Starter');
+        t.equal(store.getState().message, 'Redux Starter');
+        t.end();
+    });
+
+    t.test('dispatch message change action', t => {
+        t.equal(store.getState().message, 'Redux Starter');
+        store.dispatch(actions.updateMessage('Test'));
+        t.equal(store.getState().message, 'Test');
         t.end();
     });
 });
