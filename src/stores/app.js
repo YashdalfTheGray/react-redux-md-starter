94a7d47360eebd9f8314377cf5628d84da07e2a4
YashdalfTheGray <yash.kulshrestha@gmail.com>
2016-11-20 02:18:13 -0500

    converted the app actions to thunks, I think

--- src/stores/app.js
+++ src/stores/app.js
@@ -4,20 +4,24 @@ const initialState = {
     message: 'Redux Starter'
 };
 
-const appActions = {
-    UPDATE_MESSAGE: 'update_message'
+const actions = {
+    updateMessage: message => {
+        return {
+            type: 'update_message',
+            message
+        };
+    }
 };
 
 const app = (state = initialState, action) => {
     switch (action.type) {
-        case appActions.UPDATE_MESSAGE:
+        case 'update_message':
             return { message: action.message };
         default:
             return state;
     }
 };
 
-export default {
-    store: createStore(app),
-    actions: appActions
-};
+const store = createStore(app);
+
+export default { store, actions };
