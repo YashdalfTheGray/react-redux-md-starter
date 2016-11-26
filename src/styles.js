b0988a603a6f12b2aea3afb9c2ff5e96662cd94f
YashdalfTheGray <yash.kulshrestha@gmail.com>
2016-11-26 13:19:33 +0530

    added a file for the styles

---
+++ src/styles.js
@@ -0,0 +1,17 @@
+const marginSmall = {
+    margin: '8px'
+};
+
+const marginMedium = {
+    margin: '16px'
+};
+
+const marginLarge = {
+    margin: '24px'
+};
+
+export {
+    marginSmall,
+    marginMedium,
+    marginLarge
+};
