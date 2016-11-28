8fded2de960a9ca153cf1296d0049474c8b237ff
YashdalfTheGray <yash.kulshrestha@gmail.com>
2016-11-28 11:15:18 +0530

    added more detail to the messageDisplay

--- src/components/MessageDisplay.jsx
+++ src/components/MessageDisplay.jsx
@@ -1,11 +1,43 @@
 import React from 'react';
-import { Card, CardActions, CardTitle, CardText } from 'material-ui/Card';
+import { Card, CardTitle, CardText } from 'material-ui/Card';
+import TextField from 'material-ui/TextField';
 
-import { store } from '../stores/app';
+import { store, actions } from '../stores/app';
 import { marginMedium } from '../styles';
 
-export default props => (
-    <Card style={marginMedium}>
-        <CardTitle title={store.getState().message}></CardTitle>
-    </Card>
-);
+class MessageDisplay extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            message: store.getState().message
+        };
+
+        this.unsubMessage = store.subscribe(() => {
+            this.setState({
+                message: store.getState().message
+            });
+        })
+    }
+
+    componentWillUnmount() {
+        this.unsubMessage();
+    }
+
+    render() {
+        let message;
+        return (
+            <Card style={marginMedium}>
+                <CardTitle title={this.state.message}></CardTitle>
+                <CardText>
+                    <TextField
+                        floatingLabelText="Message"
+                        fullWidth={true}
+                        onChange={e => store.dispatch(actions.updateMessage(e.target.value))} />
+                </CardText>
+            </Card>
+        );
+    }
+}
+
+export default MessageDisplay;
