1442d4e80a1cf267680904fe52b14a71760beebe
YashdalfTheGray <yash.kulshrestha@gmail.com>
2016-12-01 19:22:28 -0500

    updated the tests which were orphaned with all the changes

--- src/stores/app.spec.js
+++ src/stores/app.spec.js
@@ -1,15 +1,14 @@
 import test from 'tape';
-import app from './app';
-const { store, actions } = app;
+import { store, actions } from './app';
 
 test('app store', t => {
     t.test('gets initial state', t => {
-        t.equal(store.getState().message, 'Redux Starter');
+        t.equal(store.getState().message, 'Hello world!');
         t.end();
     });
 
     t.test('dispatch message change action', t => {
-        t.equal(store.getState().message, 'Redux Starter');
+        t.equal(store.getState().message, 'Hello world!');
         store.dispatch(actions.updateMessage('Test'));
         t.equal(store.getState().message, 'Test');
         t.end();
